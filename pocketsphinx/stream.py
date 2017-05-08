de95acdf50ff73fd909e377882528b64317dda15
Roberto Frías Bernal <roberfrias115@gmail.com>
2017-05-08 10:46:30 +0200

    Abandoned google speech api, now using pocketsphinx

---
+++ pocketsphinx/stream.py
@@ -0,0 +1,34 @@
+""""#!/usr/bin/env python
+from os import environ, path
+
+from pocketsphinx.pocketsphinx import *
+from sphinxbase.sphinxbase import *
+
+MODELDIR = "/home/frias/Documents/Innovacion/pocketsphinx/pocketsphinx-python/pocketsphinx/model"
+DATADIR = "/home/frias/Documents/Innovacion/pocketsphinx/pocketsphinx-python/pocketsphinx/test/data"
+
+# Create a decoder with certain model
+config = Decoder.default_config()
+config.set_string('-hmm', path.join(MODELDIR, 'en-us/en-us'))
+config.set_string('-lm', path.join(MODELDIR, 'en-us/en-us.lm.bin'))
+config.set_string('-dict', path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
+decoder = Decoder(config)
+
+# Decode streaming data.
+decoder = Decoder(config)
+decoder.start_utt()
+stream = open(path.join(DATADIR, 'goforward.raw'), 'rb')
+while True:
+  buf = stream.read(1024)
+  if buf:
+    decoder.process_raw(buf, False, False)
+  else:
+    break
+decoder.end_utt()
+print ('Best hypothesis segments: ', [seg.word for seg in decoder.seg()])"""
+
+from pocketsphinx import LiveSpeech
+
+speech = LiveSpeech(lm=False, keyphrase='forward', kws_threshold=1e+20)
+for phrase in speech:
+    print(phrase.segments(detailed=True))
